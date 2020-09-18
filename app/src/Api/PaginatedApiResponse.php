677699ac5a6dee08c1a3e9313096b74b0d271837
glaphire <motoryna.dariia@pdffiller.team>
2020-09-18 23:57:10 +0300

    fixing error global handling (WIP)

---
+++ app/src/Api/PaginatedApiResponse.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Api;
+
+class PaginatedApiResponse
+{
+
+}
--- app/src/EventListener/ApiExceptionSubscriber.php
+++ app/src/EventListener/ApiExceptionSubscriber.php
@@ -2,38 +2,65 @@
 
 namespace App\EventListener;
 
-use Pagerfanta\Exception\PagerfantaException;
+use Exception;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Throwable;
 
 class ApiExceptionSubscriber implements EventSubscriberInterface
 {
+    private $isDebug;
+
+    private LoggerInterface $logger;
+
+    public function __construct(bool $isDebug, LoggerInterface $logger)
+    {
+        $this->isDebug = $isDebug;
+        $this->logger = $logger;
+    }
+
     //TODO: write proper exception handling
     public function onKernelException(ExceptionEvent $event)
     {
-//        $e = $event->getThrowable();
-//        $code = 500;
-//
-//        if ($e instanceof HttpException || $e instanceof PagerfantaException) {
-//            $code = $e->getStatusCode();
+        $exception = $event->getThrowable();
+        $request = $event->getRequest();
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $statusCode = $exception->getCode();
+        } else {
+            $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+        }
+
+        if (Response::HTTP_INTERNAL_SERVER_ERROR === $statusCode) {
+            $message = 'Internal server error';
+            $this->log($exception);
+        } else {
+            $message = $exception->getMessage();
+        }
+
+        $data = [
+            'code' => $statusCode,
+            'message' => $message,
+        ];
+
+        $response = new JsonResponse(
+            $data,
+            $statusCode,
+        );
+
+        $response->headers->set('Content-Type', 'application/json');
+
+//        if (in_array('application/json', $request->getAcceptableContentTypes())) {
+//            //$response = $this->createApiResponse($e);
+//            $event->setResponse($response);
 //        }
-//
-//        $data = [
-//            'error' => $e->getMessage(),
-//            'code' => $code,
-//        ];
-//
-//        $response = new JsonResponse(
-//            $data,
-//            $code,
-//        );
-//
-//        $response->headers->set('Content-Type', 'application/json');
-//
-//        $event->setResponse($response);
+
+        $event->setResponse($response);
     }
 
     public static function getSubscribedEvents()
@@ -41,5 +68,34 @@ class ApiExceptionSubscriber implements EventSubscriberInterface
         return [
             KernelEvents::EXCEPTION => 'onKernelException',
         ];
+    }
+
+    private function log(Throwable $exception)
+    {
+        $log = [
+            'code' => $exception->getCode(),
+            'message' => $exception->getMessage(),
+            'called' => [
+                'file' => $exception->getTrace()[0]['file'],
+                'line' => $exception->getTrace()[0]['line'],
+            ],
+            'occurred' => [
+                'file' => $exception->getFile(),
+                'line' => $exception->getLine(),
+            ],
+        ];
+
+        if ($exception->getPrevious() instanceof Exception) {
+            $log += [
+                'previous' => [
+                    'message' => $exception->getPrevious()->getMessage(),
+                    'exception' => get_class($exception->getPrevious()),
+                    'file' => $exception->getPrevious()->getFile(),
+                    'line' => $exception->getPrevious()->getLine(),
+                ],
+            ];
+        }
+
+        $this->logger->error(json_encode($log));
     }
 }
