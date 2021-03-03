633f3a8e996f9f249909280f34c1df4a69f06814
glaphire <daria.motorina@gmail.com>
2021-03-03 22:26:52 +0200

    created NearEarthObjectDTO

---
+++ app/src/DTO/NearEarthObjectDTO.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\DTO;
+
+use DateTime;
+use Symfony\Component\Validator\Constraint as Assert;
+
+class NearEarthObjectDTO
+{
+    /**
+     * @Assert\Date
+     * @Assert\NotBlank
+     */
+    public DateTime $date;
+
+    /**
+     * @Assert\Type(type="integer")
+     * @Assert\Positive
+     * @Assert\NotBlank
+     */
+    public int $reference;
+
+    /**
+     * @Assert\Type(type="string")
+     * @Assert\Length(min = 1, max = 255)
+     * @Assert\NotBlank
+     */
+    public string $name;
+
+    /**
+     * @Assert\Positive
+     * @Assert\NotBlank
+     * @Assert\Type(type="float")
+     */
+    public float $speed;
+
+    /**
+     * @Assert\Type(
+     *     type="boolean",
+     *     message="The value {{ value }} is not a valid {{ type }}."
+     * )
+     */
+    public bool $isHazardous = false;
+
+    /**
+     * @param string $date
+     * @param $reference
+     * @param $name
+     * @param $speed
+     * @param bool $isHazardous
+     */
+    public function __construct(string $date, int $reference, string $name, float $speed, bool $isHazardous = false)
+    {
+        $this->date = $date;
+        $this->reference = $reference;
+        $this->name = $name;
+        $this->speed = $speed;
+        $this->isHazardous = $isHazardous;
+    }
+
+
+}
