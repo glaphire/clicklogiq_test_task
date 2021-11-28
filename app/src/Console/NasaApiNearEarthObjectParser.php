6e112bdff36861c8e75811e38ddb079be892186f
glaphire <9280486+glaphire@users.noreply.github.com>
2021-11-28 19:51:18 +0200

    refactored error logging in console commands

--- app/src/Console/NasaApiNearEarthObjectParser.php
+++ app/src/Console/NasaApiNearEarthObjectParser.php
@@ -6,7 +6,6 @@ use App\Module\NasaApiParser\Exception\NasaApiException;
 use App\Module\NasaApiParser\ParserService;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerInterface;
-use Psr\Log\LogLevel;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -49,8 +48,7 @@ class NasaApiNearEarthObjectParser extends Command implements LoggerAwareInterfa
             $this->parserService->processNearEarthObjectList();
         } catch (NasaApiException $e) {
             $output->writeln("<error>{$e->getMessage()}</error>");
-            //TODO: refactor to use "error" method
-            $this->logger->log(LogLevel::ERROR, $e->getMessage());
+            $this->logger->error($e);
 
             return Command::FAILURE;
         }
